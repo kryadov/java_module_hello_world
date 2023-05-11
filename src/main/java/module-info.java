a33e6038fbf9a5a6ae2e7d127c2ea3ba215150b4
Konstantin Ryadov <kryadov@azul.com>
2023-05-11 11:15:50 +0400

    added vulnerable JndiLookup + ObjectMapper class using

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module main.app {
 //    requires hello.modules;
     requires com.fasterxml.jackson.databind;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j;
 }
--- src/main/java/you/shouldknowit/modules/main/MainApp.java
+++ src/main/java/you/shouldknowit/modules/main/MainApp.java
@@ -4,17 +4,27 @@ package you.shouldknowit.modules.main;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.core.lookup.JndiLookup;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainApp {
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) throws IOException {
+        var l = new JndiLookup();
+        try {
+            System.out.printf("JndiLookup: %s\n", l.lookup("a"));
+        } catch (Exception e) {
+            System.out.println("JndiLookup catch!");
+        }
+        //
         ObjectMapper mapper = new ObjectMapper();
         var srcMap = new HashMap<String, String>();
         srcMap.put("a", "b");
         var json = mapper.writeValueAsString(srcMap);
         var map = mapper.readValue(json, Map.class);
-        System.out.printf("Result is: %s", map.get("a"));
+        System.out.printf("Result is: %s\nPress Enter to exit!", map.get("a"));
+        System.in.read();
     }
 }
